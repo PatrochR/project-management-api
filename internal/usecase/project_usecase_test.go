f0183d44b78e7d868d2c741e9877471bf97bccda
reza charsetad <rezacharsetad@outlook.com>
2025-07-25 22:52:56 +0330

    refactor: enhance project use case tests and remove unused fake repositories

--- internal/usecase/project_usecase_test.go
+++ internal/usecase/project_usecase_test.go
@@ -1,36 +1,63 @@
 package usecase
 
 import (
+	"errors"
 	"testing"
+	"time"
 
-	"github.com/patorochr/project-management-api/test"
+	"github.com/patorochr/project-management-api/internal/entity"
+	"github.com/patorochr/project-management-api/mocks"
 	"github.com/stretchr/testify/assert"
 )
 
+type getByOwnerIdInput struct {
+	ownerId int
+}
+
 var getByOwnerIdCases = []struct {
-	name  string
-	input struct {
-		ownerId int
-	}
-	expect  any
-	wantErr bool
+	name        string
+	input       getByOwnerIdInput
+	mockSetup   func(projectMock *mocks.ProjectRepository, projectMemberMock *mocks.ProjectMemberRepostiroy)
+	expectErr   error
+	expectCalls bool
 }{
-	{name: "happyPath", input: struct{ ownerId int }{ownerId: 1}, expect: nil, wantErr: false},
+	{name: "success", input: getByOwnerIdInput{ownerId: 1}, expectErr: nil, expectCalls: true, mockSetup: func(projectMock *mocks.ProjectRepository, projectMemberMock *mocks.ProjectMemberRepostiroy) {
+		projectMock.On("GetByOwnerId", 1).Return(&[]entity.Project{
+			{
+				Id:          1,
+				Name:        "project",
+				Description: "project's description",
+				Owner:       1,
+				CreatedAt:   time.Now().UTC(),
+			},
+		}, nil)
+	}},
+	{name: "db error", input: getByOwnerIdInput{ownerId: 1}, expectErr: errors.New("db error"), expectCalls: true, mockSetup: func(projectMock *mocks.ProjectRepository, projectMemberMock *mocks.ProjectMemberRepostiroy) {
+		projectMock.On("GetByOwnerId", 1).Return(nil, errors.New("db error"))
+	}},
 }
 
 func TestGetOwnerId(t *testing.T) {
-	for _, c := range getByOwnerIdCases {
-		t.Run(c.name, func(t *testing.T) {
+	for _, tc := range getByOwnerIdCases {
+		t.Run(tc.name, func(t *testing.T) {
 			assert := assert.New(t)
-			fpr := test.NewFakeProjectRepository()
-			fpmr := test.NewFakeProjectMemberRepository()
-			uc := NewProjectUseCase(fpr, fpmr)
-			projects, err := uc.GetByOwnerId(c.input.ownerId)
-			if c.wantErr {
+			projectMock := mocks.NewProjectRepository(t)
+			projectMemberMock := mocks.NewProjectMemberRepostiroy(t)
+			tc.mockSetup(projectMock, projectMemberMock)
+			uc := NewProjectUseCase(projectMock, projectMemberMock)
+			projects, err := uc.GetByOwnerId(tc.input.ownerId)
+			if tc.expectErr != nil {
 				assert.Error(err)
+				assert.Nil(projects)
 			} else {
 				assert.NoError(err)
 				assert.NotNil(projects)
+			}
+
+			if tc.expectCalls {
+				projectMock.AssertExpectations(t)
+			} else {
+				projectMock.AssertNotCalled(t, "GetByOwnerId")
 			}
 
 		})
